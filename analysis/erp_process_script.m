44520b18d4dd50559f2f3482714dca643c973234
Herrick Fung <61229856+herrickfung@users.noreply.github.com>
2021-03-07 21:49:00 +0800

    Add files via upload

--- analysis/erp_process_script.m
+++ analysis/erp_process_script.m
@@ -1,24 +1,55 @@
-% Create a function at the beginning to manage paths
-InFilePath = 'C:\Users\Herrick Fung\Desktop\jacky_pilot\Jacky_session_';
-OutPPFilePath = 'C:\Users\Herrick Fung\Desktop\jacky_pilot\preprocessed\';
-OutMergedPath = 'C:\Users\Herrick Fung\Desktop\jacky_pilot\mergedset.set';
-OutSortedPath = 'C:\Users\Herrick Fung\Desktop\jacky_pilot\sorted\';
-BehavioralPath = 'C:\Users\Herrick Fung\Desktop\jacky_pilot\behavioral\20210222130124_999_jjj_ep_experiment.csv';
-
 % Main Function Runs here.
-PREPROCESSALL(InFilePath, OutPPFilePath);
-MERGEALL(OutPPFilePath, OutMergedPath);
-SORTEPOCH(BehavioralPath, OutMergedPath, OutSortedPath);
+cd 'C:\Users\Herrick Fung\Desktop\Course Materials\Sem 4.1\PSY402 Research Thesis II\experiment_part2\result\eeglab2020_0';
+eeglab
+cd '..';
+
+% Create Core Paths
+CurrentPath = pwd;
+RawPath = fullfile(CurrentPath, "raw");
+PreProcessedPath = fullfile(CurrentPath, "preprocessed");
+MergedPath = fullfile(CurrentPath, "merged");
+SortedPath = fullfile(CurrentPath, "sorted");
+CorePaths = {PreProcessedPath, MergedPath, SortedPath};
+for i = 1:length(CorePaths)
+    if not(isfolder(CorePaths{i}))
+        mkdir(CorePaths{i});
+    end
+end
+
+% Create Parti Paths and Loop All Functions
+Parti_List = dir(RawPath);
+Parti_List(1:2) = [];
+for i = 1 : length(Parti_List)
+    % Create All Required Folders
+    InPPPath = fullfile(RawPath, Parti_List(i).name);
+    BehavioralPath = fullfile(InPPPath, "behavioral", "*.csv");
+    OutPPFilePath = fullfile(PreProcessedPath, Parti_List(i).name);
+    OutMergedPath = fullfile(MergedPath, Parti_List(i).name);
+    OutSortedPath = fullfile(SortedPath, Parti_List(i).name);
+    PartiPaths = {OutPPFilePath, OutMergedPath, OutSortedPath};
+    for j = 1: length(PartiPaths)
+        if not(isfolder(PartiPaths{j}))
+            mkdir(PartiPaths{j});
+        end
+    end
+    OutMergedPath = append(OutMergedPath, '\merge.set');
+
+    % Loop All Functions
+    PREPROCESSALL(InPPPath, OutPPFilePath);
+    MERGEALL(OutPPFilePath, OutMergedPath);
+    SORTEPOCH(BehavioralPath, OutMergedPath, OutSortedPath);
+end
 
 disp('all done');
 close all;
 clear all;
 
-function PREPROCESSALL(InFilePath, OutPPFilePath)
-    for i = 1:6
-        InFileName = append(InFilePath, num2str(i),'.raw');
-        PreProed = PrePro(InFileName);
-        OutFileName = append(OutPPFilePath, 'Session_', num2str(i), 'preprocessed.set');
+function PREPROCESSALL(InPPPath, OutPPFilePath)
+    for k = 1:6
+        InPPName = append(InPPPath, '\session_', num2str(k),'.raw');
+        disp(InPPName)
+        PreProed = PrePro(InPPName);
+        OutFileName = append(OutPPFilePath, '\session_', num2str(k), '_preprocessed.set');
         PreProed = pop_saveset(PreProed, 'filename', OutFileName);
         clear PreProed;
     end
@@ -41,8 +72,9 @@ function OutEEG = PrePro(InFileName)
     EEG = pop_eegfiltnew(EEG, [], 40, [], false, [], false);
 
     % Import Channel Info
-    EEG = pop_chanedit(EEG, 'lookup','C:\\Users\\Herrick Fung\\Desktop\\jacky_pilot\\GSN-HydroCel-257.sfp',...
-        'load',{'C:\\Users\\Herrick Fung\\Desktop\\jacky_pilot\\GSN-HydroCel-257.sfp',...
+    EEG = pop_chanedit(EEG, 'lookup',...
+        'C:\\Users\\Herrick Fung\\Desktop\\Course Materials\\Sem 4.1\\PSY402 Research Thesis II\\experiment_part2\\result\\GSN-HydroCel-257.sfp',...
+        'load',{'C:\\Users\\Herrick Fung\\Desktop\\Course Materials\\Sem 4.1\\PSY402 Research Thesis II\\experiment_part2\\result\\GSN-HydroCel-257.sfp',...
         'filetype','sfp'},'delete',257);
 
     % Remove bad channels with clean_rawdata()
@@ -82,10 +114,10 @@ end
 
 
 function MERGEALL(OutPPFilePath, OutMergedPath)
-    for j = 1:5
-        InFileName = append(OutPPFilePath, 'Session_', num2str(j), 'preprocessed.set');
+    for m = 1:6
+        InFileName = append(OutPPFilePath, '\session_', num2str(m), '_preprocessed.set');
         LoadFile = pop_loadset(InFileName);
-        if j == 1
+        if m == 1
             MergedSet = LoadFile;
         else
             MergedSet = pop_mergeset(MergedSet, LoadFile, true);
@@ -98,7 +130,9 @@ end
 
 
 function SORTEPOCH(BehavioralPath, OutMergedPath, OutSortedPath)
-    TrialList = readtable(BehavioralPath, 'Format', 'auto');
+    BehavioralFileName = dir(BehavioralPath);
+    BehavioralFileName = append(BehavioralFileName.folder, '\', BehavioralFileName.name);
+    TrialList = readtable(BehavioralFileName, 'Format', 'auto');
     
     trial_con = struct('single',[TrialList.Trial_No, TrialList.Condition == 1],...
                        'lowSD', [TrialList.Trial_No, TrialList.Condition == 2],...
@@ -107,9 +141,9 @@ function SORTEPOCH(BehavioralPath, OutMergedPath, OutSortedPath)
     
     fn = fieldnames(trial_con);
     
-    for k = 1:numel(fn)
-        trial_con.(fn{k}) = find(trial_con.(fn{k})(:,2)==1);
-        trial_con.(fn{k}) = reshape(trial_con.(fn{k}), 1, 252);
+    for n = 1:numel(fn)
+        trial_con.(fn{n}) = find(trial_con.(fn{n})(:,2)==1);
+        trial_con.(fn{n}) = reshape(trial_con.(fn{n}), 1, 252);
     end
     
     FullEpochs = pop_loadset(OutMergedPath);
@@ -120,9 +154,9 @@ function SORTEPOCH(BehavioralPath, OutMergedPath, OutSortedPath)
     
     efn = fieldnames(SortedEpochs);
     
-    for m = 1:numel(efn)
-        OutFileName = append(OutSortedPath, efn{m}, '.set');
-        SortedEpochs.(efn{m}) = pop_saveset(SortedEpochs.(efn{m}), 'filename', OutFileName);
+    for o = 1:numel(efn)
+        OutFileName = append(OutSortedPath, efn{o}, '.set');
+        SortedEpochs.(efn{o}) = pop_saveset(SortedEpochs.(efn{o}), 'filename', OutFileName);
     end
    
     clear FullEpochs SortedEpochs       
